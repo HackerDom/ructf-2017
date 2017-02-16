cd1e19838069d41b7b48d53aec5d7beaf80bc25d
Maxim Muzafarov <m.muzafarov@gmail.com>
2017-02-16 13:17:43 +0300

    Use leveldb for capterka

--- services/capter/capterka/app.go
+++ services/capter/capterka/app.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/syndtr/goleveldb/leveldb"
 	"net/http"
 	"os"
 )
 
 func main() {
-	kapterka := *NewKapterka()
-	http.HandleFunc("/", kapterka.Index)
+	db, err := leveldb.OpenFile("capterka.db", nil)
+	defer db.Close()
+	http.HandleFunc("/get/", Get)
+	http.HandleFunc("/put/", Put)
 	port := "8081"
 	if len(os.Args) > 1 {
 		port = os.Args[1]
