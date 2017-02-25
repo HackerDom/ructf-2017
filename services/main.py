68036641ac2454adfed5124958e938dd7006b334
rx00 <it.tec@me.com>
2017-02-25 22:52:49 +0500

    + user model, + api beginings

--- services/main.py
+++ services/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
+from flask_restful import reqparse, Resource
 
 
 app = Flask(__name__)
@@ -7,6 +8,12 @@ app = Flask(__name__)
 @app.route("/api/register", methods=["POST"])
 def registration():
     return "It will be here, a few commits later..."
+
+
+class OrganisationInfoApi(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title')
 
 
 app.run(host="0.0.0.0", port=8080)
