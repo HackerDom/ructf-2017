206a0dfa8909dfd338315e054a13bd996974aca5
me <me@ructf>
2017-04-11 20:46:02 +0200

    redbutton: texture now uses allocator and some fixes

--- services/redbutton/service.cpp
+++ services/redbutton/service.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "glwrap.h"
-
+#include "allocator.h"
 #include "httpserver.h"
 #include "requesthandler.h"
 
@@ -24,8 +24,9 @@ GLfloat vUv[] = {   0.0f, 1.0f,
 //
 int main(int argc, char *argv[])
 {
+    InitAllocator();
     InitEGL();
-
+#if 1
 	TemplateStorage templates("templates");
 	DetectorStorage detectors("detectors");
 
@@ -37,91 +38,19 @@ int main(int argc, char *argv[])
 	getchar();
 
 	server.Stop();
+#endif
 
-    ShutdownEGL();
-	return 0;
-
-    VertexShader vs( "shaders/simple.vert", false );
-    FragmentShader fs( "shaders/checkboard.frag", false );
-    Program pr( vs, fs );
-    pr.SetAttribute( "v_pos", 3, GL_FLOAT, GL_FALSE, 0, vVertices, 6 * 3 * sizeof( GLfloat ) );
-    pr.SetAttribute( "v_uv", 2, GL_FLOAT, GL_FALSE, 0, vUv, 6 * 2 * sizeof( GLfloat ) );
-
-    /*const int C = 4;
-    Texture2D* texture[ C ];
-    for( int i = 0; i < C; i++ )
-        texture[ i ] = new Texture2D( 2, 2, FORMAT_RGBA );
-
-    for( int i = 0; i < C; i++ )
-    {
-        pr.SetVec4( "color", Vec4( 1.0f - ( float )i * 0.25f, ( float )i * 0.25f, 0.0f, 1.0f ) );
-        BindFramebuffer( *texture[ i ] );
-        Clear( 0.0, 0.0, 0.0, 0.0 );
-
-        SetProgram( pr );
-        glDrawArrays( GL_TRIANGLES, 0, 6 );
-
-        Image image0;
-        ReadPixels( image0 );
-        char buf[ 256 ];
-        memset( buf, 0, 256 );
-        sprintf( buf, "image%d.png", i );
-        save_png( buf, image0 );
-    }
-
-    for( int i = 0; i < C; i++ )
-        delete texture[ i ];
-
-    int W = 4096;
-    int H = 4096;
-    for( int i = 0; i < C; i++ )
-        texture[ i ] = new Texture2D( W, H, FORMAT_RGBA );
-
-    //
-    for( int i = 0; i < C; i++ )
-    {
-        Texture2D target( W, H, FORMAT_RGBA );
-
-        VertexShader vs( "shaders/simple.vert" );
-        FragmentShader fs_copy( "shaders/copy.frag" );
-        Program pr_copy( vs, fs_copy );
-        pr_copy.SetTexture( "tex", *texture[ i ] );
-        pr_copy.SetAttribute( "v_pos", 3, GL_FLOAT, GL_FALSE, 0, vVertices, 6 * 3 * sizeof( GLfloat ) );
-        pr_copy.SetAttribute( "v_uv", 2, GL_FLOAT, GL_FALSE, 0, vUv, 6 * 2 * sizeof( GLfloat ) );
-
-        BindFramebuffer( target );
-        Clear( 0.0, 0.0, 0.0, 0.0 );
-
-        SetProgram( pr_copy );
-        glDrawArrays( GL_TRIANGLES, 0, 6 );
-
-        //
-        Image image1;
-        ReadPixels( image1 );
-        char buf[ 256 ];
-        memset( buf, 0, 256 );
-        sprintf( buf, "imageX%d.png", i );
-        save_png( buf, image1 );
-
-        BindFramebuffer( target );
-        Clear( 0.0, 0.0, 0.0, 0.0 );
-
-        BindFramebuffer( *texture[ i ] );
-        Clear( 0.0, 0.0, 0.0, 0.0 );
-    }
-
-    for( int i = 0; i < C; i++ )
-        delete texture[ i ];*/
-
+#if 0
     /////
     {
         Image cr;
-        read_png( "crosses.png", cr );
+        read_png( argv[ 1 ], cr );
         Texture2D crTex( cr );
 
         VertexShader vs( "shaders/simple.vert", false );
-        FragmentShader fs_flag( "shaders/flag.bin.non-prerotate", true );
-        Program pr_flag( vs, fs_flag );
+        FragmentShader fs( "shaders/flag.frag", false );
+        
+        Program pr_flag( vs, fs );
         pr_flag.SetTexture( "tex", crTex );
         pr_flag.SetAttribute( "v_pos", 3, GL_FLOAT, GL_FALSE, 0, vVertices, 6 * 3 * sizeof( GLfloat ) );
 
@@ -140,17 +69,67 @@ int main(int argc, char *argv[])
         ReadPixels( flagImage );
         save_png( "flag.png", flagImage );
 
-        /*for( int i = 0; i < W; i++ ) {
+        for( int i = 0; i < W; i++ ) {
             printf( "%u %u %u %u\n", flagImage.rgba[ i ].r, flagImage.rgba[ i ].g, 
             flagImage.rgba[ i ].b, 
             flagImage.rgba[ i ].a );
-        }*/
+        }
         uint8_t* bytes = ( uint8_t* )flagImage.rgba;
         for( int i = 0; i < W * 4; i++ ){
             printf( "%02X", bytes[ i ] );
         }
         printf( "\n" );
     }
+#endif
+
+#if 0
+    {
+        FILE* f = fopen( "crosses.png", "r" );
+        fseek( f, 0, SEEK_END );
+        size_t fileSize = ftell( f );
+        fseek( f, 0, SEEK_SET );
+        char* fileData = new char[ fileSize ];
+        memset( fileData, 0, fileSize );
+        fread( fileData, 1, fileSize, f );
+        fclose( f );
+
+        Texture2D source( fileData, fileSize );
+        Texture2D target( source.GetWidth(), source.GetHeight(), FORMAT_RGBA );
+
+        VertexShader vs( "shaders/simple.vert", false );
+        FragmentShader fs_copy( "shaders/copy.frag", false );
+        Program pr_copy( vs, fs_copy );
+        pr_copy.SetTexture( "tex", source );
+        pr_copy.SetAttribute( "v_pos", 3, GL_FLOAT, GL_FALSE, 0, vVertices, 6 * 3 * sizeof( GLfloat ) );
+        pr_copy.SetAttribute( "v_uv", 2, GL_FLOAT, GL_FALSE, 0, vUv, 6 * 2 * sizeof( GLfloat ) );
+
+        //
+        BindFramebuffer( target );
+        Clear( 0.0, 0.0, 0.0, 0.0 );
+        SetProgram( pr_copy );
+        glDrawArrays( GL_TRIANGLES, 0, 6 );
+
+        //
+        RGBA* rgba = nullptr;
+        target.ReadBack( rgba );
+        save_png( "copy_test.png", rgba, target.GetWidth(), target.GetHeight() );
+    }
+#endif
+
+#if 0
+    {
+        FILE* f = fopen( "../../sploits/redbutton/test.png", "r" );
+        fseek( f, 0, SEEK_END );
+        size_t fileSize = ftell( f );
+        fseek( f, 0, SEEK_SET );
+        char* fileData = new char[ fileSize ];
+        memset( fileData, 0, fileSize );
+        fread( fileData, 1, fileSize, f );
+        fclose( f );
+
+        Texture2D test( fileData, fileSize );
+    }
+#endif
 
     ShutdownEGL();
     return 0;
