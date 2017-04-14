e64ba05d4b57210b6fd27cc018145bcfca39bca5
Tinkturianec <zaynullin-bulat@yandex.ru>
2017-04-14 15:43:27 +0500

    settings service: whitelist of methods

--- services/settings/processrequest.cbl
+++ services/settings/processrequest.cbl
@@ -5,6 +5,17 @@
        working-storage section.
        01 argc binary-long unsigned.
        01 need-more picture 9.
+       01 ind picture 9.
+
+       01 commands-list.
+         02 commands.
+           03 filler picture x(11) value 'add-section'.
+           03 filler picture x(11) value 'add-apikey '.
+           03 filler picture x(11) value 'fix-section'.
+           03 filler picture x(11) value 'get-section'.
+         02 filler redefines commands.
+           03 command-name picture x(11) occurs 4 times.
+
 
        linkage section.
        01 buffer.
@@ -32,17 +43,24 @@
             subtract function length(command) from argc end-subtract
             subtract 1 from argc end-subtract
 
-            call command using
-              by reference argc
-              by reference args
-              by reference write-buffer
-              by reference output-length
-              returning need-more
-              on exception
-                move 'uc' to write-buffer
-                move 2 to output-length
-                move 0 to need-more
-            end-call
+            move 'uc' to write-buffer
+            move 2 to output-length
+            move zero to need-more
+            perform
+              varying ind from 1 by 1 until ind is greater than 4
+              if command-name(ind) is equal to command
+                call command using
+                  by reference argc
+                  by reference args
+                  by reference write-buffer
+                  by reference output-length
+                  returning need-more
+                  on exception
+                    move 'fl' to write-buffer
+                    move 2 to output-length
+                    move zero to need-more
+                end-call
+            end-perform
 
             if need-more is greater than zero
               display 'need more argv' end-display
