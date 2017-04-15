96518b57a81e61f7cbfcc74380477379314395f2
Dmitriy Titarenko <mail.dscheg@gmail.com>
2017-04-15 20:41:44 +0500

    stargate checker random names + vulns

--- checkers/stargate/src/rnd/RndUtil.cs
+++ checkers/stargate/src/rnd/RndUtil.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace checker.rnd
 {
 	internal static class RndUtil
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Choice<T>(params T[] array) => array[ThreadStaticRnd.Next(array.Length)];
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static char Choice(string str) => str[ThreadStaticRnd.Next(str.Length)];
+
 		public static Random ThreadStaticRnd => rnd ?? (rnd = new Random(Guid.NewGuid().GetHashCode()));
 
 		public static Task RndDelay(int max) => Task.Delay(ThreadStaticRnd.Next(max));
